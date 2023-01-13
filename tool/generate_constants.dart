e47a7844402a91fb517e854b7bca5871666c7284
Rexios <rexios@rexios.dev>
2023-01-13 12:44:59 -0500

    Making things nicer

--- tool/generate_constants.dart
+++ tool/generate_constants.dart
@@ -1,31 +1,30 @@
-/// https://stackoverflow.com/a/63417082/8174191
+/// Based on https://stackoverflow.com/a/63417082/8174191
 
-import 'dart:io' show File, Process, ProcessResult;
-import 'dart:convert' show json;
+import 'dart:io';
+import 'dart:convert';
 
-void main() {
-  Process.run('flutter', ['--version', '--machine']).then(
-    (ProcessResult results) {
-      final result = Map<String, Object>.from(
-        json.decode(results.stdout.toString()) as Map,
-      );
-      final declarations = constantDeclarationsFromMap(result, 'kFlutter');
-      final file = File('lib/constants.dart');
-      file.writeAsStringSync(declarations);
-    },
-  );
+void main() async {
+  final result = await Process.run('flutter', ['--version', '--machine']);
+  final json = jsonDecode(result.stdout);
+  final declarations = [
+    ...constantDeclarationsFromMap(json, 'kFlutter'),
+    ...constantDeclarationsFromMap({
+      'buildDate': DateTime.now().toIso8601String(),
+    })
+  ].join('\n');
+  File('lib/constants.dart').writeAsStringSync(declarations);
 }
 
-String constantDeclarationsFromMap(Map<String, Object> map,
-    [String prefix = 'k']) {
+Iterable<String> constantDeclarationsFromMap(
+  Map<String, dynamic> map, [
+  String prefix = 'k',
+]) {
   String capitalize(String text) =>
-      text.isEmpty ? text : "${text[0].toUpperCase()}${text.substring(1)}";
+      text.isEmpty ? text : '${text[0].toUpperCase()}${text.substring(1)}';
 
   String constantName(String name, String prefix) =>
       prefix.isEmpty ? name : prefix + capitalize(name);
 
   return map.entries
-      .map((e) =>
-          'const ${constantName(e.key, prefix)} = ${json.encode(e.value)};')
-      .join('\n');
+      .map((e) => "const ${constantName(e.key, prefix)} = '${e.value}';");
 }
