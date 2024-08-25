6beb89da449195680e9cf345924c8f74367e970e
Rexios80 <Rexios80@users.noreply.github.com>
2024-08-25 00:30:40 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- master/flutter_bootstrap.js
+++ master/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"c94651c33b0e8064179d5b976d9b9fe24da3fb94","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"571403d88131a7ee6b176668a161c44caa7f6cd3","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2898559342"
+    serviceWorkerVersion: "2895152936"
   }
 });
