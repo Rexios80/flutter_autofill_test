d70162e6f1dcf935aa89eb2b41fac8273faf98df
Rexios80 <Rexios80@users.noreply.github.com>
2024-09-01 00:34:41 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- beta/flutter_bootstrap.js
+++ beta/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2312297628"
+    serviceWorkerVersion: "772557136"
   }
 });
