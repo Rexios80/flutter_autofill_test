396a14ea015bd90ed6a3d90eaa3252608630445a
Rexios80 <Rexios80@users.noreply.github.com>
2025-11-30 00:41:31 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- master/flutter_bootstrap.js
+++ master/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a49352746449f37f9219e25a9852d3465dc3fe5c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"2b5fa9462ce9599152ae3c7cc1a0b8b5c63eed0c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2564844664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2721145343" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
