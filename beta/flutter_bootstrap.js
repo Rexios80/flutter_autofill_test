fee1237188b4623abf69b16d4fda2602907c02b7
Rexios80 <Rexios80@users.noreply.github.com>
2025-12-14 00:41:55 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- beta/flutter_bootstrap.js
+++ beta/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b5cdf57426107e7a8eebf469a017833e013894
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2335774709" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2973629192" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
