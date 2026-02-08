9616bd7e81b1306fcc4c37b8d9d752018e646a9d
Rexios80 <Rexios80@users.noreply.github.com>
2026-02-08 00:57:44 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- beta/flutter_bootstrap.js
+++ beta/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"15d137647e372c59e8dcb78f670ab7686a0d10
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4283408282" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4097388725" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- master/flutter_bootstrap.js
+++ master/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"af5d2ceb93bb187b81787345d7a6c034347250f1","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"010cd4f383b4f49e9da0a9ef95ef66f64ff244d4","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2723860811" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "286274440" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
