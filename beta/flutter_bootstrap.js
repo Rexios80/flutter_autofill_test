5b3ea629377a14729d185d15d015050da7ff2dc6
Rexios80 <Rexios80@users.noreply.github.com>
2026-05-10 01:08:00 +0000

    Deploying to gh-pages from @ Rexios80/flutter_autofill_test@f6d9146fb411cec1a99d81c7f424b50695aac126 🚀

--- beta/flutter_bootstrap.js
+++ beta/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"73dc1ccd62aec198da4aefde1dae20b1167b13
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1562293604" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2976412570" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- master/flutter_bootstrap.js
+++ master/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"79557800f99214fa78d7a77d1e273d39c9077933","wasmHashes":{"chromium/canvaskit.wasm":"8bdb9cb4ffb12fe855acf8da6a62cd3894e5e39744bc94c534f4f5598c3ce328","wimp.wasm":"4bf05dd48d0c62fa958a3bc7f52e15165c6d369e813859e2fdb0725ed5f2842c","skwasm.wasm":"13320b26c736f9f0242051e763d6f79511537197091fa5d16073aaff99a12718","canvaskit.wasm":"f20dd3da4497881a96aa4e66e09c6e1406a0c40b8f9d783114e0ccdae64dc8db","skwasm_heavy.wasm":"567df4f29c1237c4c8f2c6178b37afa5eec0ff402064ca53ddc16f27c440e8ee","experimental_webparagraph/canvaskit.wasm":"6626af4d124e2b738340de19e758b6fd076e64dac0e31b4e8e86c23d4cd7c933"},"builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"1887a104cc6ebd8314487886215d853aab556889","wasmHashes":{"chromium/canvaskit.wasm":"f461b215b44a35044f5ca4f669b6c9cbb1af5c9f36c0abf055cd527cbbcb53c8","wimp.wasm":"2dc7fc269d05b8ca12c29e3ab86bf9e1b47875d4dca2c154c4052d5759307364","skwasm.wasm":"29da0e89863f9674fe2514d859dfa15d98114a248609a96686b8511f628a4941","canvaskit.wasm":"8eb03733b95926129bd282e26c65e4291ae6993316525997f34bbc0574347fe4","skwasm_heavy.wasm":"da37deec60fed7ceb6bdb8c3b2bc700e514d73e11d22753c27e029c04e35c0c8","experimental_webparagraph/canvaskit.wasm":"aa268b430244c255eff7a99ac0625cdc3e5b18c2b20d489033c4c78c9d908a14"},"builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4210460837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "226998048" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
